5a169ac5b2ae4b1dba3fdab12811704e4a3d26b5
ymougenel <yann.mougenel@gmail.com>
2022-10-24 14:58:17 +0300

    fix affichage

--- poo/tp1.py
+++ poo/tp1.py
@@ -4,5 +4,6 @@ from plateaux import Plateau1 as plateau
 # minou est une instance de type Chat
 minou = Chat(0)
 
+plateau.afficher([minou])
 # A Faire 1 : Faire avancer le chat de 3 cases
 # A Faire 2: Faire reculer le chat de 2 cases
